bf4ffa070bc540005e721e77550253940b9e7189
imigpt <imigpt777@gmail.com>
2026-01-23 16:03:24 +0530

    redeploy 4

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "542965145"
+    serviceWorkerVersion: "522884090"
   }
 });
